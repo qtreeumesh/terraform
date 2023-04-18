1698187241f2817c129011ce7f0702a1bbab8f26
qtreeumesh <umeshshankaraws@gmail.com>
2023-04-18 09:59:47 +0530

    addding new main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,9 @@
-#Declaring AWS Provider and credentials
-provider "aws" {
-  region     = "us-east-1"
-  access_key = "Access_key"
-  secret_key = "Secret_key"
-}
-#######################################################
-#Creating Production VPC with CIDR: 10.0.0.0/16
-resource "aws_vpc" "vpc" {
-    cidr_block = "10.0.0.0/16" 
-    tags = {
-        Name = "Production VPC"
-    }
-}
-output "vpcid" {
-  value = aws_vpc.vpc.id
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "demo-vpc"
+    Purpose = "Jenkins Demo"
+  }
 }
